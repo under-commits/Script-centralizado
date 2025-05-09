a28ceaef4ea6611c48ee36050db44ee2501eebff
under-commits <carlos.leite@underprotection.com.br>
2025-05-09 10:54:29 -0300

    Add files via upload

---
+++ meu-script.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Meu Script Tampermonkey
+// @namespace    http://suaempresa.com/
+// @version      1.0
+// @description  Script centralizado via SharePoint
+// @author       Você
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+console.log("Script rodando com sucesso!");
